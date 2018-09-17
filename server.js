892daf6f0753bd197280ef21804b44a7556f6544
INE12362324 <nabaraj.saha@tesco.com>
2018-09-17 18:10:23 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict"
+var express =require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+app.listen(4000, function(){
+    console.log('app is listening to 4000');
+})
